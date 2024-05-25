40415ac4c442af45c93c55d1e08fb66a2ae9add0
le0okk <140891498+le0okk@users.noreply.github.com>
2024-05-25 09:52:41 -0300

    aula_9-login
    login

--- aula_8/index.php
+++ aula_8/index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
+
 <body>
     <h1>
         Tela de login
     </h1>
-    <form action="sessao.php" method="post"></form>
+    <form action="sessao.php" method="post">
         <div>
             <label for="user">Usuário</label>
             <input type="text" name="user" id="user">
@@ -17,11 +19,14 @@
         <div>
             <label for="password">Senha</label>
             <input type="text" name="password" id="password">
-            <div>
-
-            </div class="conteiner-btn">
-                <button type="submit">Entrar</button>
-                <button type="reset">Limpar</button>
         </div>
+
+        <div class="conteiner-btn">
+            <button type="submit">Entrar</button>
+            <button type="reset">Limpar</button>
+        </div>
+    </form>
+
 </body>
+
 </html>
